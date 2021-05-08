2f6bea0c700179d122e59c3ba730e545a8a24191
Sandesh Pol <sandeshpol123@gmail.com>
2021-05-08 22:43:47 +0530

    Learn javascript

---
+++ Basic-JavaScript/tut45.js
@@ -0,0 +1,24 @@
+console.log("This is JavaScript Toturial");
+let name = "Sandy";
+// console.log(name + " Is good programmer ")
+
+let name1 = "Rose";
+let name2 = "Parker";
+let name3 = "Peter";
+let TextPara = "Good Morning";
+
+// let name1 = "Rose";
+// console.log(name1 + " Is good programmer ")
+// let name2 = "Parker";
+// console.log(name2 + " Is good programmer ")
+// let name3 = "Peter";
+// console.log(name3 + " Is good programmer ")
+
+function greet(name){
+    console.log("\n" + TextPara + " " + name)
+    console.log(name + " Is a Good Programmer")
+}
+
+greet(name1);
+greet(name2);
+greet(name3);
