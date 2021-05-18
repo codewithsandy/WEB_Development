8f021cb9ceffc1a7e7ed7d5f63557c74fe91a411
Sandesh Pol <sandeshpol123@gmail.com>
2021-05-18 13:09:04 +0530

    PHP Programming

---
+++ PHP programs/06. Swaping.php
@@ -0,0 +1,18 @@
+/* 6. Swap two number using third variable program in PHP
+Output:
+Value of a: 10 
+Value of b: 20 
+Value of a: 20 
+Value of b: 10			*/
+
+<?php
+$a=10; 
+$b=20; 
+echo "Value of a: $a</br>";
+echo "Value of b: $b</br>";        $temp=$a; 
+$a=$b; 
+$b=$temp; 
+echo "Value of a: $a</br>"; 
+echo "Value of b: $b</br>"; 
+?>
+
---
+++ PHP programs/07. Even Odd.php
@@ -0,0 +1,15 @@
+/*	7. Even Odd number program in PHP
+	Output:
+	Even number		*/
+
+<?php
+ $num=10;
+ if($num%2==0)
+ {
+ echo "Even number"; 
+ } 
+else 
+ {
+ echo "Odd number";
+  } 
+ ?>
