7f5542df95e7102d1c3895554ba2efece512fa5b
Sandesh Pol <sandeshpol123@gmail.com>
2021-05-18 13:03:41 +0530

    PHP Programming

---
+++ PHP programs/01. basic.php
@@ -0,0 +1,22 @@
+/* The php code can be embedded into html code just as an example given below -
+
+*/
+<html>
+<head>
+<title> Basic Example </title>
+</head>
+<body>
+<?php
+echo "This is php code embedded into html";
+?>
+</body>
+</html>
+
+similarly html code can be embedded in php code as follows -
+
+example.php
+<?php
+
+echo "<b>"."using html in php"."</b>";
+
+?>
