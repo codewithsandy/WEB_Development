a8c36cb4a555c65812fb12e5ea2e578062055fe1
Sandesh Pol <sandeshpol123@gmail.com>
2021-05-19 12:37:28 +0530

    PHP programming

---
+++ PHP programs/19. Combine the Array Elements.php
@@ -0,0 +1,9 @@
+19. Program to combine the array elements into a string with given delimiter.  */
+
+<?php  
+
+       $arr = array(9,1,2013);
+       $str = implode('/',$arr);  
+       echo $str;  
+
+ ?>
