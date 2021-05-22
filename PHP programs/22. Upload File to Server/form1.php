94b9abd2a8bd38cb79df1b6a0a8519493de8a663
Sandesh Pol <sandeshpol123@gmail.com>
2021-05-22 09:58:31 +0530

    PHP programming

---
+++ PHP programs/22. Upload File to Server/form1.php
@@ -0,0 +1,10 @@
+ <html>
+ <head>
+ </head> 
+ <body> 
+<form method="post"   action="upload.php" enctype="multipart/     form-data">
+ Resume : <input type="file" name="f1"> <br>
+<input type="submit" value="Upload">
+</form> 
+</body> 
+</html>
