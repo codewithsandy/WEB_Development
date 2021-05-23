9e834013fc9168f43183b09db0cdef5189087462
Sandesh Pol <sandeshpol123@gmail.com>
2021-05-23 11:20:23 +0530

    PHP programming

---
+++ PHP programs/30. Display Colors.php
@@ -0,0 +1,16 @@
+/*30. Write a PHP script which will display the colors in the following way   */
+
+<?php
+$color = array('white', 'green', 'red');
+foreach ($color as $c)
+{
+echo "$c, ";
+}
+sort($color);
+echo "<ul>";
+foreach ($color as $y)
+{
+echo "<li>$y</li>";
+}
+echo "</ul>";
+?>
